fd86cf39a2b09badd3b0a379b8f7251f51e8a7ed
JahanaSultan <cahana.aliyeva@gmail.com>
2023-04-14 09:07:06 +0400

    show password function added

--- static/assets/js/scripts.js
+++ static/assets/js/scripts.js
@@ -1,49 +1,49 @@
 const swiper = new Swiper('.main-swiper', {
-    // Optional parameters
-    direction: 'horizontal',
-    loop: true,
-    speed:200,
-    loop: false,
-    grabCursor:true,
-    preventClicks:true,
-    slidesPerView:4,
-    slidesPerGroup:1,
-    spaceBetween:20,
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+  speed: 200,
+  loop: false,
+  grabCursor: true,
+  preventClicks: true,
+  slidesPerView: 4,
+  slidesPerGroup: 1,
+  spaceBetween: 20,
 
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1.1,
     },
-    breakpoints: {
-        0:{
-            slidesPerView:1.1, 
-        },
-        420:{
-          slidesPerView:1.3,
-        },
-        480:{
-            slidesPerView:1.5
-        },
-        540:{
-          slidesPerView:1.7
-        },
-        576:{
-          slidesPerView:1.5,
-        },
-        767: {
-          slidesPerView:2.2,
-        },
-        992: {
-          slidesPerView: 3,
-        },
-        1200:{
-            slidesPerView: 3.5, 
-        },
-        1400:{
-            slidesPerView: 4,
-        }
-      }
-  });
+    420: {
+      slidesPerView: 1.3,
+    },
+    480: {
+      slidesPerView: 1.5
+    },
+    540: {
+      slidesPerView: 1.7
+    },
+    576: {
+      slidesPerView: 1.5,
+    },
+    767: {
+      slidesPerView: 2.2,
+    },
+    992: {
+      slidesPerView: 3,
+    },
+    1200: {
+      slidesPerView: 3.5,
+    },
+    1400: {
+      slidesPerView: 4,
+    }
+  }
+});
 
 // Login Form Validation
 (() => {
@@ -64,3 +64,18 @@ const swiper = new Swiper('.main-swiper', {
   })
 })()
 
+// Show password
+
+const showPassword = (button, input) => {
+
+  $(button).click(function () {
+    if ($(input).attr('type') == 'password') {
+      $(input).attr('type', 'text');
+    } else {
+      $(input).attr('type', 'password');
+    }
+  });
+}
+
+showPassword('#showHidePass', '#id_password')
+showPassword('#showHidePass2', '#id_password1')
